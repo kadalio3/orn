967609972cd3b401885fe3cd12116a80f80171ad
kadalio3 <kadal.goblog@gmail.com>
2024-10-27 20:46:29 +0700

    Update

--- auth.ts
+++ auth.ts
@@ -1,15 +1,15 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "@/lib/prisma"
-import Credentials from "next-auth/providers/credentials"
-import { SignInSchema } from "@/lib/zod"
-import { compareSync } from "bcrypt-ts"
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+import Credentials from "next-auth/providers/credentials";
+import { SignInSchema } from "@/lib/zod";
+import { compareSync } from "bcrypt-ts";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   pages: {
-    signIn: "/login"
+    signIn: "/login",
   },
   providers: [
     Credentials({
@@ -27,7 +27,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         const { email, password } = validatedFields.data;
 
         const user = await prisma.user.findUnique({
-          where: { email }
+          where: { email },
         });
 
         if (!user || !user.password) {
@@ -39,8 +39,8 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         if (!passwordMatch) return null;
 
         return user;
-      }
-    })
+      },
+    }),
   ],
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
@@ -55,6 +55,17 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         return Response.redirect(new URL("/dashboard", nextUrl));
       }
       return true;
+    },
+    
+    jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+
+    session({ session, token }) {
+      session.user.id = token.sub;
+      session.user.role = token.role;
+      return session;
     },
   },
 });
