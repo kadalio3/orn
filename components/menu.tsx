62333d2db73de9d0d3123433418c67a8438f3369
kadalio3 <kadal.goblog@gmail.com>
2024-10-28 16:07:07 +0700

    Update

--- components/menu.tsx
+++ components/menu.tsx
@@ -12,6 +12,10 @@ interface MenuProps {
 const Menu: React.FC<MenuProps> = ({ session }) => {
   const [dropdownVisible, setDropdownVisible] = useState(false);
   const toggleDropdown = () => setDropdownVisible(!dropdownVisible);
+  const handleSignInClick = () => {
+    setDropdownVisible(false);
+  }
+  
 
   return (
     <div className="flex items-center gap-3">
@@ -36,48 +40,58 @@ const Menu: React.FC<MenuProps> = ({ session }) => {
         )}
       </ul>
 
-      {session ? (
-        <div className="relative flex items-center gap-3">
-        <div
-          className="text-sm ring-2 ring-gray-200 bg-gray-100 rounded-full cursor-pointer"
-          onClick={toggleDropdown}
-        >
-          <Image
-            src={session.user?.image || "/avatar.svg"}
-            alt="avatar"
-            width={32}
-            height={32}
-            className="w-8 h-8 rounded-full"
-          />
-        </div>
-      
-        {dropdownVisible && (
-          <div className="absolute top-10 left-1/2 transform -translate-x-1/2 w-48 bg-white border border-gray-200 rounded-md shadow-lg z-10">
-            <div className="flex flex-col px-4 py-2 space-y-0 border-b border-gray-200">
-              <span className="font-semibold text-gray-500 capitalize">
-                {session.user?.name}
-              </span>
-              <span className="text-xs text-gray-400 capitalize">
-                {session.user?.role || "User"}
-              </span>
-            </div>
-            <button
-              onClick={() => signOut({ callbackUrl: "/login" })}
-              className="w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded-md"
-            >
-              Sign Out
-            </button>
-          </div>
-        )}
-      </div>      
-      ) : (
-        <Link
-          href="/login"
-          className="bg-red-400 text-white px-4 py-2 rounded-md hover:bg-red-500"
-        >
+      <div className="relative flex items-center gap-3">
+  <div
+    className="text-sm ring-2 ring-gray-200 bg-gray-100 rounded-full cursor-pointer flex items-center justify-center"
+    onClick={toggleDropdown}
+  >
+    {session ? (
+      <Image
+        src={session.user?.image || "/avatar.svg"}
+        alt="avatar"
+        width={32}
+        height={32}
+        className="w-8 h-8 rounded-full"
+      />
+    ) : (
+      <Image
+        src={"/avatar.svg"}
+        alt="avatar"
+        width={32}
+        height={32}
+        className="w-8 h-8 rounded-full"
+      />
+    )}
+  </div>
+
+  {dropdownVisible && session && (
+    <div className="absolute top-10 left-1/2 transform -translate-x-1/2 w-48 bg-white border border-gray-200 rounded-md shadow-lg z-10">
+      <div className="flex flex-col px-4 py-2 space-y-0 border-b border-gray-200">
+        <span className="font-semibold text-gray-500 capitalize">
+          {session.user?.name}
+        </span>
+        <span className="text-xs text-gray-400 capitalize">
+          {session.user?.role || "User"}
+        </span>
+      </div>
+      <button
+        onClick={() => signOut({ redirectTo: "/login" })}
+        className="w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded-md"
+      >
+        Sign Out
+      </button>
+    </div>
+  )}
+
+  {!session && dropdownVisible && (
+    <div className="absolute top-10 left-1/2 transform -translate-x-1/2 w-48 bg-white border border-gray-200 rounded-md shadow-lg z-10">
+      <Link href="/login" className="w-full block text-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded-md" onClick={handleSignInClick}>
           Sign In
-        </Link>
-      )}
+      </Link>
+    </div>
+  )}
+</div>
+
     </div>
   );
 };
